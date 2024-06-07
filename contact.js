0deacbf5e80e0b2bb22644b4405bb839988684e3
Aman Punekar <68313251+AmanPunekar@users.noreply.github.com>
2024-06-07 17:11:57 +0530

    UI complete with toggle btn

--- contact.js
+++ contact.js
@@ -17,3 +17,31 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   };
 });
+
+function change() {
+  let icon = document.getElementsByClassName("menu-icon")[0];
+  let btn = document.getElementsByClassName("menu-button")[0];
+  let header = document.getElementsByClassName("header-content")[0];
+  let menuBtn = document.getElementsByClassName("nav")[0];
+  const logo = document.getElementById("studynow_logo");
+
+  if (menuBtn.style.display === "flex") {
+    menuBtn.style.display = "none";
+    icon.src = "./images/Hamburger.svg";
+    icon.style.width = "2rem";
+    icon.style.height = "2rem";
+    logo.style.display = "flex";
+    header.style.flexDirection = "row";
+    btn.style.position = "sticky";
+  } else {
+    menuBtn.style.display = "flex";
+    menuBtn.style.flexDirection = "column";
+    icon.src = "./images/wrong.png";
+    icon.style.width = "2.8rem";
+    icon.style.height = "2.8rem";
+    logo.style.display = "none";
+    header.style.flexDirection = "column-reverse";
+    btn.style.position = "relative";
+    btn.style.left = "7rem";
+  }
+}
